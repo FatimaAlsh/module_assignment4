818132d6014e2fb5dde059b15277bb82facb2d1e
FatimaAlsh <82310818+FatimaAlsh@users.noreply.github.com>
2021-05-26 19:28:27 +1000

    cmt

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ assignment.
 WARNING!!! WARNING!!!
 
 */
-
+(function (){
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
@@ -40,7 +40,19 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+/* fill in parts of the 'for' loop to loop over names array */
+
+for (var i=0; i<names.length;i++){
+    if (names[i].charAt(0).toLowerCase()=='j'){
+    byeSpeaker.speak(names[i]);
+    }
+   else {
+     helloSpeaker.speak(names[i]);
+   }    
+ }
+
+  })();
+  
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -56,9 +68,9 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+  //if (/* fill in condition here */) {
     // byeSpeaker.xxxx
-  } else {
+  //} else {
     // helloSpeaker.xxxx
-  }
-}
+  //}
+//}
